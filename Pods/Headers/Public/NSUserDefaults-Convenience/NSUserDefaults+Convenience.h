2a9dd1f0ecf7b340f6caf110fcef951a6726aeac
Alejandro Iván <alejandroivan@ingenieros.com>
2017-07-31 18:51:56 -0400

    Integración de archivos de CocoaPods

---
+++ Pods/Headers/Public/NSUserDefaults-Convenience/NSUserDefaults+Convenience.h
@@ -0,0 +1,1 @@
+../../../NSUserDefaults-Convenience/src/NSUserDefaults+Convenience.h
---
+++ Pods/Target Support Files/NSUserDefaults-Convenience/NSUserDefaults-Convenience-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NSUserDefaults_Convenience : NSObject
+@end
+@implementation PodsDummy_NSUserDefaults_Convenience
+@end
